979db1414e18fd81fa50b2952af1eb1f2a03c9e9
guiijoo <gui.marchetto@hotmail.com>
2024-08-12 16:54:42 -0300

    interfaces

---
+++ src/iphone/aplicativos/NavegadorInternet.java
@@ -0,0 +1,9 @@
+package iphone.aplicativos;
+
+public interface NavegadorInternet {
+
+    public void exibirPagina(String url);
+    public void adicionarNovaAba();
+    public void atualizarPagina();
+    
+}
---
+++ src/iphone/aplicativos/ReprodutorMusial.java
@@ -0,0 +1,9 @@
+package iphone.aplicativos;
+
+public interface ReprodutorMusial {
+
+    public void tocar();
+    public void pausar();
+    public void selecionarMusica(String musica);
+    
+}
